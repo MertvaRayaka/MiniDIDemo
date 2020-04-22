23b43d5bf83cbad35004c1687ec1a202a91af5cf
NEIL.ZHU <631203498@qq.com>
2020-04-22 20:02:15 +0800

    添加项目文件。

---
+++ MiniDIDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniDIDemo
+{
+    /// <summary>
+    /// 本项目演示DI注入原理
+    /// </summary>
+    class Program
+    {
+        
+        //public static IUser _user; //不用di容器调用，会出现未将对象引用设置到实例的异常，容器就是为了初始化实例；
+        public static ICar _car;
+        //public Program(IUser user)
+        //{
+        //    _user = user;
+        //}
+
+        public Program(ICar car)
+        {
+            _car = car;
+        }
+        static void Main(string[] args)
+        {
+            //1. 创建容器；
+            SampleDI container = new SampleDI();
+            //2. 注册服务；
+            container.Register<IUser, UsersService>();
+            container.Register<ICar, CarService>();
+
+            //3. 从容器中获取对象；
+
+            // 基于接口编程
+            IUser user = container.GetService<IUser>();
+            user.MyName("peter");
+            user.MyCarName();
+
+            IServiceCollection Container = new ServiceCollection();
+            Container.AddScoped<IUser,UsersService>().AddScoped<ICar,CarService>();
+            var provider = Container.BuildServiceProvider();
+            var User = provider.GetService<IUser>();
+            User.MyName("Neil");
+            User.MyCarName();
+
+
+            Console.WriteLine("接口调用完毕！");
+            Console.ReadKey();
+        }
+    }
+}
